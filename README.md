e4b153b2aabd69eb3a84503fe36a2446ca3ceddb
Rajeshwari Bhaskaran <raji.89@icloud.com>
2025-07-31 00:27:46 +0100

    Clean Terraform configuration - no large files

---
+++ README.md
@@ -0,0 +1,4 @@
+This project creates a robust two-tier architecture on AWS using Terraform, featuring an Application Load Balancer (ALB) distributing traffic across two web servers deployed in different Availability Zones for high availability.
+
+Start by modifying variables in var.tf for security groups, subnets, keys, AMI ID, region, availability zones, and VPC to establish an AWS ELB
+
---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "aws_vpc" {
+  description = "VPC"
+  default     = "vpc-************"
+}
+
+variable "aws_subnet" {
+  description = "aws subnet"
+  default     = "subnet-*********"
+}
+
+variable "aws_subnet2" {
+  description = "aws subnet2"
+  default     = "subnet-*******"
+}
+
+variable "aws_key" {
+  description = "aws key"
+  default     = "****"
+}
+
+variable "aws_ami" {
+  description = "ami id"
+  default     = "ami-0175d4f2509d1d9e8"
+}
